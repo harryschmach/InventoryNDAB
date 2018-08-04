3de3a952daf88a0c83648baf05fe7df7a41f8688
harryschmach <harrisons@sandiego.edu>
2018-08-04 10:48:39 -0700

    Initial commit. Setup some db stuff. need more

---
+++ app/src/main/java/com/harry/inventoryndab/data/ProductContract.java
@@ -0,0 +1,64 @@
+package com.harry.inventoryndab.data;
+
+import android.provider.BaseColumns;
+
+public final class ProductContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private ProductContract() {}
+
+    /**
+     * Inner class that defines constant values for the inventory database table.
+     * Each entry in the table represents a single product.
+     */
+    public static final class ProductEntry implements BaseColumns {
+
+        /** Name of database table for products */
+        public final static String TABLE_NAME = "products";
+
+        /**
+         * Unique ID number for the product (only for use in the database table).
+         *
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+
+        /**
+         * Name of the product.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_PRODUCT_NAME ="name";
+
+        /**
+         * Price of the product. INTEGER because we can divide by 100 later ;)
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_PRODUCT_PRICE = "price";
+
+        /**
+         * Quantity of the product.
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_PRODUCT_QUANTITY = "quantity";
+
+        /**
+         * Supplier name of the product.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_SUPPLIER_NAME = "supplier_name";
+
+        /**
+         * Supplier phone number of the product.
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_SUPPLIER_PHONE_NUMBER = "supplier_phone_number";
+    }
+
+}
+
