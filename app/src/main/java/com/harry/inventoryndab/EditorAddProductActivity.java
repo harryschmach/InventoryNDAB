6e8332b537edb08af4f2609884cfa4d907d231be
harryschmach <harrisons@sandiego.edu>
2018-08-06 23:36:09 -0700

    Add sql insertion to editor activity. Phone number is string since int is too small for 10 digits. Figures.

--- app/src/main/java/com/harry/inventoryndab/EditorAddProductActivity.java
+++ app/src/main/java/com/harry/inventoryndab/EditorAddProductActivity.java
@@ -1,13 +1,34 @@
 package com.harry.inventoryndab;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.harry.inventoryndab.data.InventoryDbHelper;
+import com.harry.inventoryndab.data.ProductContract;
+
+import static com.harry.inventoryndab.data.ProductContract.*;
 
 public class EditorAddProductActivity extends AppCompatActivity {
+
+    /**
+     * Instantiate variables of edit text fields
+     */
+    EditText mProductName;
+    EditText mProductPrice;
+    EditText mProductQuantity;
+    EditText mSupplierName;
+    EditText mSupplierPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +38,88 @@ public class EditorAddProductActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        /**
+         * Cast variables of edit text fields
+         */
+        mProductName = findViewById(R.id.et_product_name);
+        mProductPrice = findViewById(R.id.et_product_price);
+        mProductQuantity = findViewById(R.id.et_product_quant);
+        mSupplierName = findViewById(R.id.et_supplier_name);
+        mSupplierPhone = findViewById(R.id.et_supplier_phone);
     }
 
+    private void insertProduct(){
+        // Read from input fields
+        // Use trim to eliminate leading or trailing white space
+        String productNameString = mProductName.getText().toString().trim();
+        String productPriceString = mProductPrice.getText().toString().trim();
+        float productPrice = Float.parseFloat(productPriceString);
+        String productQuantString = mProductQuantity.getText().toString().trim();
+        int productQuantity = Integer.parseInt(productQuantString);
+        String supplierNameString = mSupplierName.getText().toString().trim();
+        String supplierPhoneString = mSupplierPhone.getText().toString().trim();
+//        int supplierPhone = Integer.parseInt(supplierPhoneString);
+
+
+        // Create database helper
+        InventoryDbHelper mDbHelper = new InventoryDbHelper(this);
+
+        // Gets the database in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a ContentValues object where column names are the keys,
+        // and product attributes from the editor are the values.
+        ContentValues values = new ContentValues();
+        values.put(ProductEntry.COLUMN_PRODUCT_NAME, productNameString);
+        values.put(ProductEntry.COLUMN_PRODUCT_PRICE, productPrice);
+        values.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, productQuantity);
+        values.put(ProductEntry.COLUMN_SUPPLIER_NAME, supplierNameString);
+        values.put(ProductEntry.COLUMN_SUPPLIER_PHONE_NUMBER, supplierPhoneString);
+
+        // Insert a new row for pet in the database, returning the ID of that new row.
+        long newRowId = db.insert(ProductEntry.TABLE_NAME, null, values);
+
+        // Show a toast message depending on whether or not the insertion was successful
+        if (newRowId == -1) {
+            // If the row ID is -1, then there was an error with insertion.
+            Toast.makeText(this, "Error with saving product", Toast.LENGTH_SHORT).show();
+        } else {
+            // Otherwise, the insertion was successful and we can display a toast with the row ID.
+            Toast.makeText(this, "Product saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Save" menu option
+            case R.id.action_insert_editor_data:
+                // Save pet to database
+                insertProduct();
+                // Exit activity
+                finish();
+                return true;
+            // Respond to a click on the "Delete" menu option
+            case R.id.action_delete_all_entries:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Up" arrow button in the app bar
+            case android.R.id.home:
+                // Navigate back to parent activity (MainActivity)
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
